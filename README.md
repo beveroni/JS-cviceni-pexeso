5133b266ffc789ebfe4adb849b48c9a772d4ea6e
Veronika Bendova <126337758+beveroni@users.noreply.github.com>
2023-04-11 19:42:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pexeso
+
+Postupujte podle zadání cvičení [Pexeso](https://kodim.cz/kurzy/daweb/js1/cykly/cv-foreach#cvlekce%3Epexeso).
---
+++ app.js
@@ -0,0 +1,5 @@
+console.log('Funguju!')
+
+const karticky = document.querySelectorAll('.karticka')
+
+karticky[0].classList.remove('otocena')
